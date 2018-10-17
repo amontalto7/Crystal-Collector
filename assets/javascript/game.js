3364e2f8e24fc729d423febad956b844454d3c5f
ant4u <anthonyimontalto@gmail.com>
2018-10-17 19:08:27 -0400

    attempted to make array of unique numbers. not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,63 @@
 var counter = 0;
-var targetNumber = 53;
 var wins = 0;
 var losses = 0;
+
+function getRandom(min,max) {
+    //This function takes two integers- a mininum and a maximum #, and returns a random number within that range
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+// create an array "numberOptions" which contains 4 randomly generated numbers between 1 and 12
+// var numberOptions = [10, 5, 3, 7];
+
+function resetArray(num){
+    var ints = [];
+
+    while (ints.length < num ){
+        var r = getRandom(1,12);
+        if (!ints.indexOf(r) > -1) {
+            ints.push(r);
+        }
+    }
+    return ints;
+}
+
+var numberOptions = resetArray(4);
+console.log(numberOptions);
+var numberOptions = resetArray(4);
+console.log(numberOptions);
+var numberOptions = resetArray(4);
+console.log(numberOptions);
+var numberOptions = resetArray(4);
+console.log(numberOptions);
+var numberOptions = resetArray(4);
+console.log(numberOptions);
+
+
+
+
+var crystalgame = {
+    generateTargetNumber : function() {     // generate a random number between 19 and 120
+        return getRandom(19,120); 
+    },
+
+    reset : function() {
+        targetNumber = crystalgame.generateTargetNumber();
+        $("#randomNum").text(targetNumber);
+        counter = 0;
+        $("#totalscore").text(counter);
+
+    }
+
+}
+
+var targetNumber = crystalgame.generateTargetNumber();
 
 $("#randomNum").text(targetNumber);
 $("#wins").text(wins);
 $("#losses").text(losses);
 
-var numberOptions = [10, 5, 3, 7];
+
 
 for (var i = 0; i < numberOptions.length; i++) {
   // For each iteration, we will create an imageCrystal
@@ -29,8 +79,6 @@ for (var i = 0; i < numberOptions.length; i++) {
   $("#crystalContainer").append(imageCrystal);
 }
 
-var increment = numberOptions[Math.round(Math.random())];
-
 $(".crystal-image").on("click", function() {
 // grab the value from the data-crystalvalue attribute on the image clicked
     var crystalValue = ($(this).attr("data-crystalvalue"));
@@ -40,7 +88,6 @@ $(".crystal-image").on("click", function() {
     // Every click, from every crystal adds to the global counter.
     counter += crystalValue;
 
-    // counter += increment;
   $("#totalscore").text(counter);
 
   if (counter == targetNumber) {
