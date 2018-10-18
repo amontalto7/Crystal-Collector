f36bfd1e00a56472f69da8a2a2686ca58e106403
ant4u <anthonyimontalto@gmail.com>
2018-10-18 13:54:04 -0400

    Finished!

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# unit-4-game
+# Crystal Collector game :gem:
+## unit-4-game 
+### Columbia Engineering Full Stack Bootcamp - HomeWork #4
+
+This is a game written with **jQuery**, **Javascript**, **HTML** and **CSS**.
+
+You will be given a random number at the start of the game.
+
+There are four crystals. By clicking on a crystal you will add a specific amount of points to your total score.
+
+You win the game by matching your total score to the random number, you lose the game if your total score goes above the random number.
+
+The value of each crystal is hidden from you until you click on it.
+
+Each time the game restarts, the game will change the values of each crystal.
+
+* Assignment details located on [GitLab](https://columbia.bootcampcontent.com/columbia-bootcamp/COLNYC201809FSF2/blob/master/01_homework/week_04/homework_instructions.md)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,94 +2,84 @@ var counter = 0;
 var wins = 0;
 var losses = 0;
 
-function getRandom(min,max) {
-    //This function takes two integers- a mininum and a maximum #, and returns a random number within that range
-    return Math.floor(Math.random()*(max-min+1)+min);
+function getRandom(min, max) {
+  //This function takes two integers- a mininum and a maximum #, and returns a random number within that range
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // create an array "numberOptions" which contains 4 randomly generated numbers between 1 and 12
-// var numberOptions = [10, 5, 3, 7];
 
+function resetArray(num) {
+  // this function will generate an array of UNIQUE random numbers
+  var ints = [];
 
-function resetArray(num){
-    // this function will generate an array of UNIQUE random numbers
-    var ints = [];
-
-    while (ints.length < num ){
-        var r = getRandom(1,12);
-        if (ints.indexOf(r) === -1) {
-            ints.push(r);
-        }
+  while (ints.length < num) {
+    var r = getRandom(1, 12);
+    if (ints.indexOf(r) === -1) {
+      ints.push(r);
     }
-    return ints;
+  }
+  return ints;
 }
-
-// var numberOptions = resetArray(4);
 
 // game object
 var crystalgame = {
-    numberOptions : [],
-    generateTargetNumber : function() {     // generate a random number between 19 and 120
-        return getRandom(19,120); 
-    },
+  numberOptions: [],    // empty array that will store crystal values
+  generateTargetNumber: function() {
+    // generate a random number between 19 and 120
+    return getRandom(19, 120);
+  },
 
-    reset : function() {
-        targetNumber = this.generateTargetNumber();
-        $("#randomNum").text(targetNumber);
-        counter = 0;
-        $("#totalscore").text(counter);
-        this.numberOptions = resetArray(4);
-        console.log("inside: " + this.numberOptions);
+  reset: function() {
+    targetNumber = this.generateTargetNumber();
+    $("#randomNum").text(targetNumber);
+    counter = 0;
+    $("#totalscore").text(counter);
+    this.numberOptions = resetArray(4); // generate new numbers
+    console.log("inside: " + this.numberOptions);
 
-        // empty crystal container of old crystals
-        $("#crystalContainer").empty();
-
-        // re-generate crystals
-        for (var i = 0; i < this.numberOptions.length; i++) {
-            // For each iteration, we will create an imageCrystal
-            var imageCrystal = $("<img>");
-          
-            // Each crystal will be given the class ".crystal-image".
-            imageCrystal.addClass("crystal-image");
-          
-            // Each imageCrystal will be given a src link to a unique crystal image
-            var imgSrc = "assets/images/crystal" + i + ".png";
-            imageCrystal.attr("src", imgSrc);
-            imageCrystal.attr("alt", "Crystal"+i);
-          
-            // Each imageCrystal will be given a data attribute called data-crystalValue.
-            // This data attribute will be set equal to the array value.
-            imageCrystal.attr("data-crystalvalue", this.numberOptions[i]);
-            console.log(imageCrystal);
-            // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-            $("#crystalContainer").append(imageCrystal);
-          }
-          
+    // re-assign the new attributes to the crystal images
+    for (var i = 0; i < this.numberOptions.length; i++) {
+      $("#Crystal" + i).attr("data-crystalvalue", this.numberOptions[i]);
     }
-
-}
-
-// var targetNumber = crystalgame.generateTargetNumber();
-// $("#randomNum").text(targetNumber);
-
+  }
+};
 
 $("#wins").text(wins);
 $("#losses").text(losses);
 
 crystalgame.reset();
 
+// generate crystals
+for (var i = 0; i < crystalgame.numberOptions.length; i++) {
+  // For each iteration, we will create an imageCrystal
+  var imageCrystal = $("<img>");
 
+  // Each crystal will be given the class ".crystal-image".
+  imageCrystal.addClass("crystal-image");
 
-console.log($(".crystal-image"));
+  // Each imageCrystal will be given a src link to a unique crystal image
+  var imgSrc = "assets/images/crystal" + i + ".png";
+  imageCrystal.attr("src", imgSrc);
+  imageCrystal.attr("alt", "Crystal" + i);
+  imageCrystal.attr("id", "Crystal" + i);
+
+  // Each imageCrystal will be given a data attribute called data-crystalValue.
+  // This data attribute will be set equal to the array value.
+  imageCrystal.attr("data-crystalvalue", crystalgame.numberOptions[i]);
+  console.log(imageCrystal);
+  // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+  $("#crystalContainer").append(imageCrystal);
+}
+
 $(".crystal-image").on("click", function() {
-// grab the value from the data-crystalvalue attribute on the image clicked
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
+  // grab the value from the data-crystalvalue attribute on the image clicked
+  var crystalValue = $(this).attr("data-crystalvalue");
+  crystalValue = parseInt(crystalValue);
 
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
-    console.log("CrystalValue: " + crystalValue);
+  // Every click, from every crystal adds to the global counter.
+  counter += crystalValue;
+  console.log("CrystalValue: " + crystalValue);
 
   $("#totalscore").text(counter);
 
